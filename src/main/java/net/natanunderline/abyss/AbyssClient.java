ba373063f95e4e2c1b3b9c3cf42b7f4e3bbb1c2d
Firstname Lastname <ghitotuzi.2018@gmail.com>
2024-10-20 12:49:56 -0300

    Initial commit

---
+++ src/main/java/net/natanunderline/abyss/AbyssClient.java
@@ -0,0 +1,10 @@
+package net.natanunderline.abyss;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbyssClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
